1bed14c0f6d33dba6fb5f0a8b5ccfe6798d8aeb3
amano-honmono <maplehuke@yahoo.co.jp>
2017-03-06 12:19:58 +0900

    UsersのCRUDを作成

--- contest_server/app/controllers/users_controller.rb
+++ contest_server/app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:user_name)
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user_params = params.require(:user).permit(:user_name)
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_url, notice: 'User was successfully destroyed.'
+    else
+      render :new
+    end
+  end
 end
