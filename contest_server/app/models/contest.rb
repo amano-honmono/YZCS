9361dbe915feac715c81416089d23ed4bafbfc9f
amano-honmono <maplehuke@yahoo.co.jp>
2017-03-08 00:04:15 +0900

    ContestをProblemsと紐づけ

--- contest_server/app/models/contest.rb
+++ contest_server/app/models/contest.rb
@@ -4,6 +4,7 @@ class Contest < ApplicationRecord
   validates :start_time, presence: true
   validates :ending_time, presence: true
   validate :start_end_check
+  has_and_belongs_to_many :problems
 
   def start_end_check
     errors.add(:ending_time, "ending time is invalid.") unless
