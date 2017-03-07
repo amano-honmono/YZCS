067121877b54f1adf18b22d2a5229eac1ea49fb7
amano-honmono <maplehuke@yahoo.co.jp>
2017-03-07 21:34:00 +0900

    contestのモデルを更新

--- contest_server/app/models/contest.rb
+++ contest_server/app/models/contest.rb
@@ -1,2 +1,12 @@
 class Contest < ApplicationRecord
+  validates :contest_name, presence: true
+  validates :contest_name, uniqueness: true
+  validates :start_time, presence: true
+  validates :ending_time, presence: true
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:ending_time, "ending time is invalid.") unless
+    self.start_time < self.ending_time
+  end
 end
--- contest_server/test/models/contest_test.rb
+++ contest_server/test/models/contest_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ContestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @contest = Contest.new(contest_name: "aaaa", start_time: DateTime.now, ending_time: DateTime.now + 1)
+  end
+
+  test "contest_name should not be empty" do
+    @contest.contest_name = ""
+    assert_not @contest.valid?
+  end
+
+  test "contest_name shoule be unique" do
+    duplicate_contest = @contest.dup
+    @contest.save
+    assert_not duplicate_contest.valid?
+  end
+
+  test "ending_time should be later than start_time" do
+    @contest.ending_time = DateTime.now - 1
+    assert_not @contest.valid?
+  end
 end
