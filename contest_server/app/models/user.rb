bcb51bac51431026eedf17f8e8a93d8d3c176394
amano-honmono <maplehuke@yahoo.co.jp>
2017-03-07 20:41:26 +0900

    Userのモデルを更新

--- contest_server/app/models/user.rb
+++ contest_server/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :user_name, presence: true
+  validates :user_name, uniqueness: true
+  has_many :contest
 end
--- contest_server/test/models/user_test.rb
+++ contest_server/test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(user_name: "aaaa")
+  end
+
+  test "user_name should not be empty" do
+    @user.user_name = ""
+    assert_not @user.valid?
+  end
+
+  test "user_name shoule be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
