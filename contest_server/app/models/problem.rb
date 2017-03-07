48f6870f60404c62a035aecc12c584b0bf41de5c
amano-honmono <maplehuke@yahoo.co.jp>
2017-03-08 00:03:37 +0900

    Problemのmodelを更新

--- contest_server/app/models/problem.rb
+++ contest_server/app/models/problem.rb
@@ -1,2 +1,12 @@
 class Problem < ApplicationRecord
+  VALID_URL_REGEX = /\Ahttp:\/\/judge.u-aizu.ac.jp\/onlinejudge\/description.jsp?.*\z/ #とりあえずAOJにのみ対応
+  validates :problem_url, presence: true, uniqueness: true, format: {with: VALID_URL_REGEX}
+  validate :check_url
+  has_and_belongs_to_many :contests
+
+  def check_url
+    # 渡されたURLがvalidなものか判定する
+    # そのうち実装する
+    true
+  end
 end
--- contest_server/test/models/problem_test.rb
+++ contest_server/test/models/problem_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ProblemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @problem = Problem.new(problem_url: "aaaa")
+  end
+
+  test "problem_url should not be empty" do
+    @problem.problem_url = ""
+    assert_not @problem.valid?
+  end
+
+  test "problem_url shoule be unique" do
+    duplicate_problem = @problem.dup
+    @problem.save
+    assert_not duplicate_problem.valid?
+  end
+
+  test "AOJ url should be valid" do
+    @problem.problem_url = "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1147&lang=jp"
+    assert @problem.valid?
+  end
+
+  test "problem_url should be AOJ" do
+    @problem.problem_url = "http://google.com"
+    assert_not @problem.valid?
+  end
 end
